301dc945d0bb0caa134057873ebfcc29375b74af
IgorLark <sochiy1300@gmail.com>
2020-11-10 23:30:37 +0300

    brows.html

--- test_test.py
+++ test_test.py
@@ -1,7 +1,6 @@
-my_list=input("Вводим только цифры: ")
+my_list = input("Вводим только цифры: ")
 if my_list.isdigit():
     print("только цифры")
 else:
     print("Текст недопустим")
-#заверщение
 print("OK")
